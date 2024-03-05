e7d477f8be6dee79209b05497fd2744075fd8ad6
Idaretos <dragonkevin@snu.ac.kr>
2024-03-05 14:40:09 +0900

    remove default file directory

--- Time_Cost_Trade_Off/preprocessing.py
+++ Time_Cost_Trade_Off/preprocessing.py
@@ -12,11 +12,14 @@ def read_files(filepath) -> pd.DataFrame:
     costs.drop(columns=['overhead_cost'], inplace=True)
     return workflow, costs, overhead_cost
 
-def arg(inputpath=PATH+'/input/example', rule='heuristic', v=False) -> tuple:
+def arg(inputpath=None, rule='heuristic', v=False) -> tuple:
     if len(sys.argv) > 1:
         inputpath = sys.argv[1]
     if len(sys.argv) > 2:
         rule = sys.argv[2]
     if len(sys.argv) > 3:
         v = ('true' in sys.argv[3].lower())
+    if inputpath is None:
+        print('Please provide the input path')
+        sys.exit()
     return inputpath, rule, v
